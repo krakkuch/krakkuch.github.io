1f21baf67a02f5325590682853bed56012bc6f44
krakkuch <115030383+krakkuch@users.noreply.github.com>
2022-11-19 15:39:03 +0300

    ДЗ 11-12

--- source/js/calc.js
+++ source/js/calc.js
@@ -58,45 +58,48 @@
 //}
 //calc();
 
-let site, design, adaptive;
 
-let calculator ={
-    price: [
-        [3000, 6000, 4000],
-        [3000, 5000, 3000],
-        [2000, 2000, 4000],
-    ],
-    days: [
-    [2, 4, 3],
-    [3, 3, 4],
-    [2, 3, 3],
-    ],
-    run(siteType, designType, adaptiveType){
-    let sum = parceInt(calculator.price[0][siteType-1]) + parceInt(calculator.price[1][designType-1]) + parceInt(calculator.price [2][adaptiveType-1]);
-    let days = parceInt(calculator.days[0][siteType-1]) + parceInt(calculator.days[1][designType-1]) + parceInt(calculator.days [2][adaptiveType-1]);
-    alert("Сроки: "+ days + " Стоимость: "+ sum);
-}
-}
+--
 
-function getAnswers() {
-site = prompt("Введите тип сайта: \n1 - Визитка, \n2 - Корпоративный, \n3 - Интернет-магазин");
-    if(site != 1 && site != 2 && site != 3){
-        alert("Такого варианты нет");
-        getAnswers();
-        return;
-    }
-design = prompt("Выберите дизайн: \n1 - Шаблонный, \n2 - Уникальный, \n3 - WOW-Дизайн");
-    if(design != 1 && design != 2 && design != 3){
-        alert("Такого варианты нет");
-        getAnswers();
-        return;
-    }
-adaptive = prompt("Адаптивность: \n1 - Только ПК, \n2 - Только Мобильная версия, \n3 - ПК + Мобильный");
-        if(design != 1 && design != 2 && design != 3){
-        alert("Такого варианты нет");
-        getAnswers();
-        return;
-    }
-        calculator.run(site, design, adaptive);
-}
-getAnswers();
+//let site, design, adaptive;
+//
+//let calculator ={
+//    price: [
+//        [3000, 6000, 4000],
+//        [3000, 5000, 3000],
+//        [2000, 2000, 4000],
+//    ],
+//    days: [
+//    [2, 4, 3],
+//    [3, 3, 4],
+//    [2, 3, 3],
+//    ],
+//    run(siteType, designType, adaptiveType){
+//    let sum = parseInt(calculator.price[0][siteType-1]) + parseInt(calculator.price[1][designType-1]) + parseInt(calculator.price [2][adaptiveType-1]);
+//    let days = parseInt(calculator.days[0][siteType-1]) + parseInt(calculator.days[1][designType-1]) + parseInt(calculator.days [2][adaptiveType-1]);
+//    alert("Сроки: "+ days + " Стоимость: "+ sum);
+//}
+//}
+//
+//function getAnswers() {
+//site = prompt("Введите тип сайта: \n1 - Визитка, \n2 - Корпоративный, \n3 - Интернет-магазин");
+//    if(site != 1 && site != 2 && site != 3){
+//        alert("Такого варианты нет");
+//        getAnswers();
+//        return;
+//    }
+//design = prompt("Выберите дизайн: \n1 - Шаблонный, \n2 - Уникальный, \n3 - WOW-Дизайн");
+//    if(design != 1 && design != 2 && design != 3){
+//        alert("Такого варианты нет");
+//        getAnswers();
+//        return;
+//    }
+//adaptive = prompt("Адаптивность: \n1 - Только ПК, \n2 - Только Мобильная версия, \n3 - ПК + Мобильный");
+//        if(design != 1 && design != 2 && design != 3){
+//        alert("Такого варианты нет");
+//        getAnswers();
+//        return;
+//    }
+//        calculator.run(site, design, adaptive);
+//}
+//getAnswers();
