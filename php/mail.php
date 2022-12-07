c02d85ccf80fb5769eebf4e9d5dc7e6b76793fff
krakkuch <115030383+krakkuch@users.noreply.github.com>
2022-12-08 02:20:44 +0300

    дз

--- php/mail.php
+++ php/mail.php
@@ -9,6 +9,13 @@ $phone = $_POST['user_phone'];
 $text = $_POST['user_message'];
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'testvisitsite@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'iRORtPa13ao#'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
 $mail->setFrom('testvisitsite@mail.ru'); // от кого будет уходить письмо?
 $mail->addAddress('vipper27@mail.ru');     // Кому будет уходить письмо 
